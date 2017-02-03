eb944171e9c747388364c5aa21cd56aef1337332
Kabegami <lucas.becirspahic@gmail.com>
2017-02-03 18:32:12 +0100

    update

--- Projet1/projet.py
+++ Projet1/projet.py
@@ -25,6 +25,52 @@ class Grille(object):
         pt.title("Bataille navale TAVU")
         pt.show()
 
+    def peut_placer(self,bateau, position, direction):
+    
+        for i in range(bateau.taille):
+            if (direction == 'h'):
+                x = position[0] + i
+                y = position[1]
+            else:
+                x = position[0]
+                y = position[1] + i
+            if x >= self.taille or y >= self.taille or x < 0 or y < 0:
+                return False
+            if self.matrice[x,y] != 0:
+                return False
+        return True
+
+    def place(self,bateau, position, direction):
+        #Attention le bateau est toujours placer vers la droite ou le bas par convention
+        if not(self.peut_placer(bateau, position , direction)):
+            print("impossible de placer le bateau renvoi l'ancienne grille")
+        else:
+            for i in range(bateau.taille):
+                if (direction == 'h'):
+                    x = position[0] + i
+                    y = position[1]
+                else:
+                    x = position[0]
+                    y = position[1] + i
+                self.matrice[x,y] = bateau.idB
+
+    def place_alea(self,bateau):
+        x = random.randint(0,self.taille-1)
+        y = random.randint(0,self.taille-1)
+        direction = ['h','v']
+        indDir = direction[random.randint(0,1)]
+        return self.place(bateau,(x,y), indDir)
+
+    def enleve(self,bateau, position, direction):
+        for i in range(bateau.taille):
+            if (direction == 'h'):
+                x = position[0] + i
+                y = position[1]
+            else:
+                x = position[0]
+                y = position[1] + i
+            self.matrice[x,y] = 0
+
 class Bateau(object):
     def __init__(self,nom="RAOUL",idB=1,taille=1):
         self.taille = taille
@@ -33,52 +79,6 @@ class Bateau(object):
 
     def __repr__(self):
         return "nom : {} , taille : {}, id : {}".format(self.nom, self.taille, self.idB)
-
-class Direction(object):
-    def __init__(self, direction):
-        self.direction = direction
-        if self.direction == "haut":
-            self.vecteur = np.array([0,1])
-        elif self.direction == "bas":
-            self.vecteur = np.array([0,-1])
-        elif self.direction == "droite":
-            self.vecteur = np.array([1,0])
-        else:
-            self.vecteur = np.array([-1,0])
-
-    def decalage_direction(self, position, facteur):
-        return np.array[position] + facteur*self.vecteur
-                
-    
-def peut_placer(bateau, grille, position, direction):
-    #la direction est un tuple appartenant a {(0,1): droite, (1,0):haut ,(-1,0):bas, (0,-1):gauche}
-    #Si possible faire classe Direction(nom) -> nom, vecteur comme plus haut
-    for i in range(bateau.taille):
-        x = position[0] + i*direction[0]
-        y = position[1] + i*direction[1]
-        if x >= grille.taille or y >= grille.taille or x < 0 or y < 0:
-            return False
-        if grille.matrice[x,y] != 0:
-            return False
-    return True
-
-def place(bateau, grille, position, direction):
-    if not(peut_placer(bateau, grille, position , direction)):
-        print("impossible de placer le bateau renvoi l'ancienne grille")
-        return grille
-    else:
-        for i in range(bateau.taille):
-            x = position[0] + i*direction[0]
-            y = position[1] + i*direction[1]
-            grille.matrice[x,y] = bateau.idB
-        return grille
-    
-def place_alea(bateau, grille):
-    x = random.randint(0,9)
-    y = random.randint(0,9)
-    direction = [(0,1), (0,-1),(1,0),(-1,0)]
-    indDir = direction[random.randint(0,3)]
-    return place(bateau, grille,(x,y), indDir)
 
 
 def liste_bateaux():
@@ -94,12 +94,51 @@ def genere_grille():
     g = Grille(10)
     L = liste_bateaux()
     for b in L:
-        place_alea(b,g)
+        g.place_alea(b)
     return g
     
+#Partie Combinatoire du jeu
+def nb_facon_grille(G,b):
+    #Attention la fonction necessite python 3 sinon utiliser les sets
+    cpt = 0
+    direction = ['h','v']
+    for i in range(0,G.taille):
+        for j in range(0,G.taille):
+            for d in direction:
+                if G.peut_placer(b,(i,j),d):
+                    cpt += 1
+    return cpt
 
-
+def nb_place_kBateau(G,L):
+    #fait 2 tour de boucles
+    nbgrille = 0
+    direction = ['h','v']
+    for bateau in L:
+        for i in range(0,G.taille):
+            for j in range(0,G.taille):
+                for d in direction:
+                    if G.peut_placer(bateau,(i,j),d):
+                       G.place(bateau,(i,j),d)
+                       nbgrille += nb_facon_grille(G,bateau)
+                       print((i,j),d)
+                       G.enleve(bateau,(i,j),d)
+                       
+                    
+                        
+    return nbgrille
 
 # if __name__ == "__main__":
 g = genere_grille()
 g.show()
+g2 = Grille(2)
+L = liste_bateaux()
+test = []
+test.append(L[-1])
+test.append(L[-1])
+#test.append(L[-2])
+
+print(test)
+cpt = nb_facon_grille(g2,test[0])
+errorincoming = nb_place_kBateau(g2,test)
+print(cpt)
+print(errorincoming)
